4a12fe20f403f656b7671cf2eb494a9c05955713
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2020-10-31 00:12:41 -0300

    Create model parent class

---
+++ examples/common/model.py
@@ -0,0 +1,35 @@
+import json
+
+
+class SpecialDefault:
+    pass
+
+
+class Model:
+    uuid: str
+
+    def __init__(self, **kwargs):
+        data = {}
+        for field in self.__dataclass_fields__.values():
+            data[field.name] = kwargs.get(field.name, field.default)
+
+        for key, value in data.items():
+            if isinstance(value, SpecialDefault):
+                value = value.get(data)
+
+            setattr(self, key, value)
+
+    def to_json(self):
+        data = {'uuid': self.uuid}
+        for field in self.__dataclass_fields__:
+            data[field] = getattr(self, field)
+
+        return json.dumps(data)
+
+    @classmethod
+    def from_json(cls, data):
+        data = json.loads(data)
+        uuid = data.pop('uuid', None)
+        obj = cls(**data)
+        obj.uuid = uuid
+        return obj
