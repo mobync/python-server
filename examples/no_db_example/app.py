3e7a352fd63bcecf41a728b19a6d562c6491e99e
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2020-10-31 01:03:15 -0300

    Create no db example

---
+++ examples/no_db_example/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, session, redirect, url_for, request, abort, jsonify
+
+from examples.no_db_example.models import Task
+from sync import Sync
+from examples.relational_db_example.implementation import Implementation
+from os import listdir
+from os.path import isfile, join
+
+app = Flask(__name__)
+
+implementation = Implementation()
+sync = Sync(implementation)
+
+db = {}
+
+
+@app.route('/sync', methods=['POST'])
+def sync():
+    data = request.get_json()
+
+    if sync.is_valid(data):
+        resp = sync.apply(data)
+        return jsonify(resp)
+    else:
+        abort(400, sync.error_message)
+
+
+@app.route('/list-tasks', methods=['GET'])
+def list_tasks():
+    resp = [task.to_dict() for task in db['tasks']]
+    return jsonify(resp)
+
+
+def load_mock_data():
+    path = 'example_data'
+    folder = 'tasks'
+    files = [join(path, folder, f) for f in listdir(join(path, folder))]
+
+    tasks = []
+    for file in files:
+        f = open(file)
+        tasks.append(Task.from_json(f.read()))
+        f.close()
+
+    db['tasks'] = tasks
+
+
+if __name__ == '__main__':
+    load_mock_data()
+    app.run()
