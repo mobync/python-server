5abe6f87e68c49a0e7246a0520a7a070bb186956
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2020-10-31 01:04:49 -0300

    Initiate config files to relational example

---
+++ examples/relational_db_example/implementation.py
@@ -0,0 +1,22 @@
+from synchronizer import Synchronizer
+
+
+class Implementation(Synchronizer):
+
+   def __init__(self):
+      pass
+
+   def read(self):
+      pass
+
+   def update(self):
+      pass
+
+   def create(self):
+      pass
+
+   def delete(self):
+      pass
+
+   def validate(self):
+      pass
